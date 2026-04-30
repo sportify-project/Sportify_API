627ff2db866f22160b011ef878cb69b0a205caa1
Thinha1 <Thinhanhdang014@gmail.com>
2026-04-30 10:28:31 +0700

    change sth

--- src/main/java/sport/store/thinh/ThinhApplication.java
+++ src/main/java/sport/store/thinh/ThinhApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ThinhApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 
 		dotenv.entries().forEach(entry -> {
 			System.setProperty(entry.getKey(), entry.getValue());
